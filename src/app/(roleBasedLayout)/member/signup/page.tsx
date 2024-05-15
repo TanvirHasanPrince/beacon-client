36a4e1d41f9fda7c7f137026527e7b9885af0c25
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-05-15 09:59:35 +0600

    feat: member signup

--- src/app/(roleBasedLayout)/member/signup/page.tsx
+++ src/app/(roleBasedLayout)/member/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useAddMemberMutation } from "@/redux/api/memberApi";
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import "../../../../helpers/envConfig";
 
@@ -10,51 +10,43 @@ const MemberSignUpPage = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-
-  // Using the useAddMemberMutation hook from Redux Toolkit
   const [addMemberMutation, { isLoading, isError, error }] =
     useAddMemberMutation();
 
-  const onSubmit = async (data: any) => {
-    console.log(data.profilePhoto[0]);
+  const onSubmit = async (data:any) => {
     const rawImage = data.profilePhoto[0];
-
     const formData = new FormData();
     formData.append("file", rawImage);
-    formData.append("upload_preset", "beaconPreset"); // Corrected typo
+    formData.append("upload_preset", "beaconPreset");
 
     try {
       const uploadResponse = await fetch(
         `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_NAME}/image/upload`,
-        {
-          method: "POST",
-          body: formData,
-        }
+        { method: "POST", body: formData }
       );
 
       if (!uploadResponse.ok) {
-        throw new Error(`Image upload failed`);
+        throw new Error("Image upload failed");
       }
 
       const imageData = await uploadResponse.json();
-      console.log(imageData.secure_url);
-
       data.profilePhoto = imageData.secure_url;
-      // Call the addMemberMutation function with the form data
+
       const response = await addMemberMutation(data);
       console.log(response);
-      // Handle success response here
     } catch (error) {
-      console.error("An error occurred:", error); // Handle error here
+      console.error("An error occurred:", error);
     }
   };
 
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto flex flex-col items-center justify-center px-12">
       <h1 className="text-2xl font-semibold mt-8 mb-4">Member Sign Up</h1>
-
-      <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto">
-        <div className="mb-4">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex items-center flex-col w-full max-w-md"
+      >
+        <div className="mb-4 w-full">
           <label className="block mb-2">First Name</label>
           <input
             type="text"
@@ -65,7 +57,7 @@ const MemberSignUpPage = () => {
             <p className="text-red-500">First Name is required</p>
           )}
         </div>
-        <div className="mb-4">
+        <div className="mb-4 w-full">
           <label className="block mb-2">Middle Name</label>
           <input
             type="text"
@@ -73,7 +65,7 @@ const MemberSignUpPage = () => {
             className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
           />
         </div>
-        <div className="mb-4">
+        <div className="mb-4 w-full">
           <label className="block mb-2">Last Name</label>
           <input
             type="text"
@@ -84,7 +76,7 @@ const MemberSignUpPage = () => {
             <p className="text-red-500">Last Name is required</p>
           )}
         </div>
-        <div className="mb-4">
+        <div className="mb-4 w-full">
           <label className="block mb-2">Email</label>
           <input
             type="email"
@@ -93,7 +85,7 @@ const MemberSignUpPage = () => {
           />
           {errors.email && <p className="text-red-500">Email is required</p>}
         </div>
-        <div className="mb-4">
+        <div className="mb-4 w-full">
           <label className="block mb-2">Mobile</label>
           <input
             type="tel"
@@ -101,7 +93,7 @@ const MemberSignUpPage = () => {
             className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
           />
         </div>
-        <div className="mb-4">
+        <div className="mb-4 w-full">
           <label className="block mb-2">Password</label>
           <input
             type="password"
@@ -112,7 +104,7 @@ const MemberSignUpPage = () => {
             <p className="text-red-500">Password is required</p>
           )}
         </div>
-        <div className="mb-4">
+        <div className="mb-4 w-full">
           <label className="block mb-2">Country</label>
           <input
             type="text"
@@ -120,18 +112,17 @@ const MemberSignUpPage = () => {
             className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
           />
         </div>
-        <div className="mb-4">
+        <div className="mb-4 w-full">
           <label className="block mb-2">Interest</label>
           <select
             {...register("interest")}
-            multiple
             className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
           >
             <option value="Football">Football</option>
             <option value="Gardening">Gardening</option>
           </select>
         </div>
-        <div className="mb-4">
+        <div className="mb-4 w-full">
           <label className="block mb-2">Bio</label>
           <textarea
             {...register("bio")}
@@ -152,12 +143,14 @@ const MemberSignUpPage = () => {
             <span className="text-red-500">Please upload a profile photo.</span>
           )}
         </div>
-        <button
-          type="submit"
-          className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-        >
-          Submit
-        </button>
+        <div className="flex justify-center ">
+          <button
+            type="submit"
+            className="mt-2 mb-8  bg-red-500 hover:bg-blue-600 text-white font-semibold py-2 px-28 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+          >
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );
