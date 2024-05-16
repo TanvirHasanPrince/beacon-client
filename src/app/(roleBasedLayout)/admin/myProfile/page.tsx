79b69dce82479137d30d9656d09d3d0f9cd42736
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-05-16 09:21:05 +0600

    feat: get admin by id and show profile

--- src/app/(roleBasedLayout)/admin/myProfile/page.tsx
+++ src/app/(roleBasedLayout)/admin/myProfile/page.tsx
@@ -1,7 +1,90 @@
+"use client";
+import { useAdminQuery } from "@/redux/api/adminApi";
+import { getUserInfo } from "@/services/auth.service";
+import {
+  FaEnvelope,
+  FaPhoneAlt,
+  FaUser,
+  FaGlobe,
+  FaInfoCircle,
+  FaClock,
+} from "react-icons/fa";
 import React from "react";
+import moment from "moment";
 
 const AdminProfilePage = () => {
-  return <div>AdminProfilePage</div>;
+  const { role, userId } = getUserInfo() as any;
+
+  const { data: adminData, isLoading, isError } = useAdminQuery(userId);
+
+  if (isLoading) {
+    return <div>Loading...</div>; // Render loading state while fetching data
+  }
+
+  if (isError || !adminData) {
+    return <div>Error occurred while fetching data.</div>; // Render error state if data fetching fails
+  }
+
+  // Destructure admin data
+  const {
+    firstName,
+    middleName,
+    lastName,
+    email,
+    mobile,
+    createdAt,
+    updatedAt,
+    // Add other fields if needed
+  } = adminData.data;
+
+  const formattedCreatedAt = moment(createdAt).format("MMMM Do YYYY, h:mm a");
+  const formattedUpdatedAt = moment(updatedAt).format("MMMM Do YYYY, h:mm a");
+
+  return (
+    <>
+      <div className="flex flex-col items-center pb-10">
+        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+          {`${firstName} ${middleName} ${lastName}`}
+        </h5>
+
+        <div className=" flex flex-col items-center justify-center overflow-hidden w-96">
+          {/* Render admin information */}
+          <div className="p-4">
+            <div className="mb-2">
+              <FaUser className="inline-block mr-2  text-red-500" />{" "}
+              {/* Icon for name */}
+              {firstName} {middleName && `${middleName} `} {lastName}
+            </div>
+            <div className="mb-2">
+              <FaEnvelope className="inline-block mr-2 text-red-500" />{" "}
+              {/* Icon for email */}
+              {email}
+            </div>
+            <div className="mb-2">
+              <FaPhoneAlt className="inline-block mr-2  text-red-500" />{" "}
+              {/* Icon for phone number */}
+              {mobile}
+            </div>
+          </div>
+
+          <div className="mb-2">
+            <span className="font-semibold">
+              <FaClock className="inline-block mr-2  text-red-500" />
+              Created on:
+            </span>{" "}
+            {formattedCreatedAt}
+          </div>
+          <div className="mb-4">
+            <span className="font-semibold">
+              <FaClock className="inline-block mr-2  text-red-500" />
+              Last updated
+            </span>{" "}
+            {formattedUpdatedAt}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AdminProfilePage;
---
+++ src/redux/api/adminApi.ts
@@ -0,0 +1,60 @@
+import { tagTypes } from "../tag-types";
+import { baseApi } from "./baseApi";
+
+const ADMIN_URL = "/admin";
+
+export const adminApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    addAdmin: build.mutation({
+      query: (data) => ({
+        url: `${ADMIN_URL}/create-admin`,
+        method: "POST",
+        data: data,
+      }),
+      invalidatesTags: [tagTypes.admin],
+    }),
+    admins: build.query({
+      query: (arg: Record<string, any>) => ({
+        url: `${ADMIN_URL}`,
+        method: "GET",
+        params: arg,
+      }),
+      transformResponse: (response) => {
+        return {
+          admins: response,
+        };
+      },
+      providesTags: [tagTypes.admin],
+    }),
+    admin: build.query({
+      query: (id) => ({
+        url: `${ADMIN_URL}/${id}`,
+        method: "GET",
+      }),
+      providesTags: [tagTypes.admin],
+    }),
+    updateAdmin: build.mutation({
+      query: (data) => ({
+        url: `${ADMIN_URL}/${data.id}`,
+        method: "PATCH",
+        data: data.body,
+      }),
+      invalidatesTags: [tagTypes.admin],
+    }),
+    deleteAdmin: build.mutation({
+      query: (id) => ({
+        url: `${ADMIN_URL}/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [tagTypes.admin],
+    }),
+  }),
+});
+
+export const {
+  useAddAdminMutation,
+  useAdminsQuery,
+  useAdminQuery,
+  useUpdateAdminMutation,
+  useDeleteAdminMutation,
+} = adminApi;
