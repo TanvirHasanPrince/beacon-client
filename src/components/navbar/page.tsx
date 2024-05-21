81b96347cab92dd4e79b131c6ace87b69f3b1f45
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-05-21 15:45:54 +0600

    chore: created browser events navbar

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -130,6 +130,13 @@ const Navbar = () => {
                   >
                     Subscribed Events
                   </Link>
+
+                  <Link
+                    href="/member/events/allEvents"
+                    className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
+                  >
+                    Browse Events
+                  </Link>
                   <Link
                     href="/member/journals/myJournals"
                     className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
