45dd3b5602b39f7c351685f40321d70e118c26b1
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-05-22 13:37:47 +0600

    fix: add Journal page

--- src/app/(roleBasedLayout)/member/journals/addJournal/page.tsx
+++ src/app/(roleBasedLayout)/member/journals/addJournal/page.tsx
@@ -1,21 +1,28 @@
 "use client";
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { useAddJournalEntryMutation } from "@/redux/api/journalApi";
 import { getUserInfo } from "@/services/auth.service";
 import { toast } from "react-hot-toast";
 import { JOURNAL_MOOD_ENUM, JOURNAL_TYPE_TAGS_ENUM } from "@/enums/sharedEnums";
 import { getSentiment } from "@/utils/getSentiment";
+import Select, { ActionMeta, MultiValue } from "react-select";
+
+import { tailwindButtonClass } from "@/components/tailwindClasses";
+import { useRouter } from "next/navigation";
 
 const AddJournalEntry = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
+    control,
     reset,
   } = useForm();
   const [addJournalEntry] = useAddJournalEntryMutation();
   const [submitting, setSubmitting] = useState(false);
+
+  const router = useRouter();
 
   const onSubmit = async (data: any) => {
     setSubmitting(true);
@@ -28,8 +35,11 @@ const AddJournalEntry = () => {
       const sentimentResult = getSentiment(data.content);
       data.sentimentResult = sentimentResult.mood;
 
+      console.log(data);
+
       await addJournalEntry(data);
       toast.success("Journal entry added successfully!");
+      router.push("/member/journals/myJournals");
       reset();
     } catch (error) {
       toast.error("Could not add journal entry");
@@ -37,6 +47,12 @@ const AddJournalEntry = () => {
     }
     setSubmitting(false);
   };
+
+  // Assuming JOURNAL_TYPE_TAGS_ENUM is an object with tag values
+  const options = Object.values(JOURNAL_TYPE_TAGS_ENUM).map((tag) => ({
+    value: tag,
+    label: tag,
+  }));
 
   return (
     <div className="container mx-auto flex flex-col items-center justify-center px-12">
@@ -90,19 +106,29 @@ const AddJournalEntry = () => {
           </select>
           {errors.mood && <p className="text-red-500">Mood is required</p>}
         </div>
+
         <div className="mb-4 w-full">
           <label className="block mb-2">Tags</label>
-          <select
-            {...register("tags", { required: true })}
-            multiple
-            className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-          >
-            {Object.values(JOURNAL_TYPE_TAGS_ENUM).map((tag) => (
-              <option key={tag} value={tag}>
-                {tag}
-              </option>
-            ))}
-          </select>
+          <Controller
+            name="tags"
+            control={control}
+            rules={{ required: true }}
+            render={({ field: { onChange, value } }) => (
+              <Select
+                isMulti
+                options={options}
+                className="w-full"
+                classNamePrefix="react-select"
+                onChange={(
+                  newValue: MultiValue<{ value: string; label: string }>
+                ) => onChange(newValue.map((option) => option.value))}
+                value={value?.map((option: string) => ({
+                  value: option,
+                  label: option,
+                }))}
+              />
+            )}
+          />
           {errors.tags && <p className="text-red-500">Tags are required</p>}
         </div>
         <div className="mb-4 w-full">
@@ -116,7 +142,7 @@ const AddJournalEntry = () => {
         <button
           type="submit"
           disabled={submitting}
-          className="mt-2 mb-8 bg-red-500 hover:bg-blue-600 text-white font-semibold py-2 px-28 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+          className={`${tailwindButtonClass}`}
         >
           {submitting ? "Adding..." : "Submit"}
         </button>
