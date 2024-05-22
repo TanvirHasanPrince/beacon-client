0795bd4cc5da0280e1b0260cac3be747a2eae86d
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-05-22 09:18:11 +0600

    fix: removed pet from navbar

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -112,12 +112,12 @@ const Navbar = () => {
                     My Profile
                   </Link>
 
-                  <Link
+                  {/* <Link
                     href="/member/pets"
                     className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
                   >
                     My Pets
-                  </Link>
+                  </Link> */}
                   <Link
                     href="/member/events/myEvents"
                     className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
